adcbe7572d7f51302530feb4472eb42565d79c8e
lasaleks <lasaleks@gmail.com>
2023-08-31 19:38:37 +0500

    add unixtime, mock unixtime

---
+++ base.go
@@ -0,0 +1,42 @@
+package ie_common_utils_go
+
+import "time"
+
+type NowUnixTimeI interface {
+	Unix() int64
+}
+
+type NowUnixTime struct {
+}
+
+func (*NowUnixTime) Unix() int64 {
+	return time.Now().Unix()
+}
+
+type MockUnixTime struct {
+	value int64
+}
+
+func (t *MockUnixTime) Unix() int64 {
+	return t.value
+}
+
+func keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func BoolToInt(v bool) int {
+	if v {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func IntToBool(v int) bool {
+	return v != 0
+}
