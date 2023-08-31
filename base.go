d251423d8fec4ffbf2da4ca04fa7ea535bf71611
lasaleks <lasaleks@gmail.com>
2023-08-31 19:42:07 +0500

    _

--- base.go
+++ base.go
@@ -21,7 +21,7 @@ func (t *MockUnixTime) Unix() int64 {
 	return t.value
 }
 
-func keys[K comparable, V any](m map[K]V) []K {
+func Keys[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
